9c718c81bd2bb256529b071a764cd39e78e6676a
Jeremy Oweh <141310678+dinduoweh@users.noreply.github.com>
2025-03-20 23:07:27 -0400

    Designed test cases testing for prime numbers, non-numbers and edge cases.

---
+++ CSCN71020-A3/UnitTestPrime/UnitTestPrime.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestPrime
+{
+	TEST_CLASS(UnitTestPrime)
+	{
+	public:
+		
+		TEST_METHOD(TEStprime)
+		{
+			Assert::AreEqual("Prime", isPrime(2));
+			Assert::AreEqual("Prime", isPrime(5));
+			Assert::AreEqual("Prime", isPrime(9));
+		}
+
+		TEST_METHOD(TEStnonprime)
+		{
+			Assert::AreEqual("Not Prime", isPrime(4));
+			Assert::AreEqual("Not Prime", isPrime(8));
+			Assert::AreEqual("Not Prime", isPrime(12));
+		}
+
+		TEST_METHOD(TEStprimeEdgeCase) 
+		{
+			Assert::AreEqual("Not Prime", isPrime(-5));
+			Assert::AreEqual("Not Prime", isPrime(0));
+			Assert::AreEqual("Not Prime", isPrime(1));
+		}
+	};
+}
